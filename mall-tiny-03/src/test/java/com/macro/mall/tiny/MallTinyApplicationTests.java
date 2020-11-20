56892c9e76b8db1b1f9e482ae9fac0966ad8a7e5
wangshoudao1 <wang_shoud@163.com>
2020-11-20 09:24:11 +0800

    测试接口

--- mall-tiny-03/src/test/java/com/macro/mall/tiny/MallTinyApplicationTests.java
+++ mall-tiny-03/src/test/java/com/macro/mall/tiny/MallTinyApplicationTests.java
@@ -1,16 +1,32 @@
 package com.macro.mall.tiny;
 
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.mbg.model.PmsBrand;
+import com.macro.mall.tiny.service.PmsBrandService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallTinyApplicationTests {
 
+    @Autowired
+    private PmsBrandService brandService;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    @ResponseBody
+    public void getListTest(){
+        System.err.println(CommonResult.success(brandService.listAllBrand()).getData());
     }
 
 }
